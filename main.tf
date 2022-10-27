ef00f3452f79142d9803aadfbe7426e24051a358
ccie7599 <bapley@akamai.com>
2022-10-27 15:50:52 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -50,11 +50,11 @@ resource "linode_lke_cluster" "foobar2" {
         }  
     }
 }
-resource "local_file" "lke_kubeconfig_yaml" {
+resource "local_file" "lke_east_yaml" {
     content  = base64decode(linode_lke_cluster.foobar1.kubeconfig)
     filename = "${path.module}/us-east.yaml"
 }
-resource "local_file" "lke_kubeconfig_yaml" {
+resource "local_file" "lke_west_yaml" {
     content  = base64decode(linode_lke_cluster.foobar2.kubeconfig)
     filename = "${path.module}/us-west.yaml"
 }
